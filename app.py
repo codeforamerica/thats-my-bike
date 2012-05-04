2d15c6c298d2baf70664c14b00ae74807117b8b0
Ruthie BenDor <ruthie@unruthless.com>
2012-05-03 21:16:26 -0700

    Made it into a Flask app (thx to @mr0grog)

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def on_submit():
+    
+    return 'has "owner_name": %s' % ('owner_name' in request.form)
+
+if __name__ == '__main__':
+    app.run(debug=True)
