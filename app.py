b2ae5359ab2c996a7b4a13fba01989c3c0c7ac33
Ruthie BenDor <ruthie@unruthless.com>
2012-05-03 22:00:49 -0700

    Now with file writing goodness

--- app.py
+++ app.py
@@ -11,8 +11,33 @@ def index():
 @app.route('/submit', methods=['POST'])
 def on_submit():
     
-    return 'has "owner_name": %s' % ('owner_name' in request.form)
+    filename = request.form['owner_name'] + '.txt'
+    
+    path = os.path.join('.','tmp',filename)
+    
+    try:
+        # Create text file.
+        f = open(path, 'w')
+        try:
+            # Write form data to the casefile
+            f.write(request.form['owner_name'])
+        finally:
+            f.close()
+    except IOError:
+        return "write failed"
+    
+    # See if it worked!
+    try:
+        f = open(path, 'r')
+        try:
+            f.read()
+        finally:
+            f.close()
+    except IOError:
+        return "read failed"
+    
+    return "all done!"
 
 if __name__ == '__main__':
-    port = int(os.environ['PORT'])
+    port = int('PORT' in os.environ and os.environ['PORT'] or '5000')
     app.run(host='0.0.0.0',port=port)
